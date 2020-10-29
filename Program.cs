3077e956ff4ad1835583ce475217c2fb118966f6
Martin Finkel <me@martinfinkel.com>
2020-10-29 16:25:01 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MSBuildBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
